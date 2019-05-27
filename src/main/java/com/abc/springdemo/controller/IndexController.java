a534cfcbbde89ce9e7cb2c9475b6866126b5419a
zhangzeqiang <zhangzeqiang@kuaishou.com>
2019-05-27 22:57:40 +0800

    web-inf

--- src/main/java/com/abc/springdemo/controller/IndexController.java
+++ src/main/java/com/abc/springdemo/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.abc.springdemo.main;
+package com.abc.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
